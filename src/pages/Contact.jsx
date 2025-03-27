afceeab593ecc8f95a13611827330aa6cdfbab6a
Ashutosh Ahire <ahireashutosh10@gmail.com>
2025-03-28 00:59:02 +0530

    Contact Added_Pending

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
+import { FaLifeRing, FaMapMarkerAlt, FaCloudUploadAlt } from "react-icons/fa";
+import "./Contact.css"; 
 
-const Contact = () => {
+const ContactSection = () => {
+  const contactInfo = [
+    {
+      icon: <FaLifeRing className="contact-icon" />,
+      title: "Support",
+      phone: "+49 - 162 910 7481",
+      email: "zerowaste@gokidogo.de",
+    },
+    {
+      icon: <FaMapMarkerAlt className="contact-icon" />,
+      title: "Address",
+      address: `House of Logistics & Mobility (HOLM) GmbH
+      Startup LAB – Go Kidogo Deutschland
+      Bessie-Coleman-Straße 7
+      60549 Frankfurt am Main`,
+    },
+    {
+      icon: <FaCloudUploadAlt className="contact-icon" />,
+      title: "Contact",
+      phone: "+49 - 0162 910 7481",
+      email: "service@gokidogo.de",
+    },
+  ];
+
   return (
     <div>
-      <h1>hello from Contact</h1>
-    </div>
-  )
-}
+      {/* Background Image Section */}
+      <div className="background-image1">
+        
+      <div className="text-content">
 
-export default Contact
+</div>
+<div className="about-text">
+<h1>Contact Us</h1>
+  <p>Connect with us</p>
+</div>
+</div>
+
+      {/* Contact Details Section */}
+      <div className="contact-section">
+        <div className="contact-container">
+          {contactInfo.map((item, index) => (
+            <div key={index} className="contact-card">
+              <div>{item.icon}</div>
+              <h3 className="contact-title">{item.title}</h3>
+              {item.address ? (
+                <p className="contact-text">{item.address}</p>
+              ) : (
+                <>
+                  <p className="contact-text">{item.phone}</p>
+                  <p className="contact-text">{item.email}</p>
+                </>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactSection;
+
+
---
+++ src/pages/Privacy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Privacy() {
+  return (
+    <div>
+      <h1>In private</h1>
+    </div>
+  )
+}
+
+export default Privacy
