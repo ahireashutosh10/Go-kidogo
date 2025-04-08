3a2d9a54ded227c5654c3e2678ac870ae8f475f5
Ashutosh Ahire <ahireashutosh10@gmail.com>
2025-04-08 18:03:17 +0530

    Add partnership images in the page

--- src/pages/GokidoTool.jsx
+++ src/pages/GokidoTool.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import './GokidoTool.css'
+import devices from "../assets/headergokidogotools.png";
+
+
 const GokidoTool = () => {
   return (
     <>
-        <div className="background-image1">
-            <div className="text-content1">
+       <div class="hero-section">
+  <div class="hero-content">
+    <div class="hero-left">
+      <img src="../assets/headergokidogotools.png" alt="Devices Preview" class="hero-img" />
+    </div>
+    <div class="hero-right">
+      <h1>Your smart companions<br />into a digital future</h1>
+      <button class="demo-btn">Request a demo now</button>
+    </div>
+  </div>
+  <div class="wave"></div>
+</div>
 
-          </div>
-          <div className="about-text1">
-          <h1>About-us</h1>
-            <p>#We proTest:How we started!..</p>
-          </div>
-        </div>
     
-    
+
     </>
   )
 }
